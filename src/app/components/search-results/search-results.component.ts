32c658df0d673e9a48634d64e42ebc5606a12e6b
Simon Davey <sales@cookiedigital.co.uk>
2019-12-12 10:35:54 +0000

    Add if search results null

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SearchService } from '../../services/search.service';
 
@@ -8,7 +8,10 @@ import { SearchService } from '../../services/search.service';
   styleUrls: ['./search-results.component.scss']
 })
 export class SearchResultsComponent implements OnInit {
+  @Input() ngIf: any;
+
   results = [];
+  keywords = '';
 
   constructor(private route: ActivatedRoute, private searchService: SearchService) {}
 
@@ -20,6 +23,7 @@ export class SearchResultsComponent implements OnInit {
   }
 
   process(keywords: any) {
+    this.keywords = keywords.charAt(0).toUpperCase() + keywords.slice(1);;
     this.searchService.getResults(keywords).subscribe(results => {
       this.results = results;
     },
